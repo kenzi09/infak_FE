fee91bc15cdd0d40e798617625711325eee0ce47
AntonWitjaksono1412 <antonwitjaksono1412@gmail.com>
2024-10-29 11:57:00 +0700

    update

--- src/Users/Dashboard2.jsx
+++ src/Users/Dashboard2.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import coin from "../assets/img/coin.jpg";
