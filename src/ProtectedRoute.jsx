57c88624deeda3f8ad1922110aa6fbb1e1054811
kenzi09 <kenzibadrika@smkwikrama.sch.id>
2024-11-15 14:23:57 +0700

    route

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,29 +1,12 @@
-// ProtectedRoute.jsx
 import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, requiredRole }) => {
   const token = sessionStorage.getItem("token");
-  const userRole = sessionStorage.getItem("role"); // Simpan role di sessionStorage setelah login
-  const location = useLocation();
+  const userRole = sessionStorage.getItem("role");
 
-  // Jika pengguna mengakses halaman login dan sudah ada token, redirect ke dashboard
-  if (location.pathname === "/login" && token || location.pathname === "/" && token) {
-    if (userRole === "Admin") 
-    {
-      return <Navigate to="/Admin/" replace />;
-    } else if (userRole === "Siswa")
-    {
-      return <Navigate to="/User/dashboard" replace />;
-    } else if (userRole === "PS")
-    {
-      return <Navigate to="/PS/" replace />;
-    }
-  }
-
-  // Cek apakah token dan role sesuai dengan role yang dibutuhkan
   if (!token || userRole !== requiredRole) {
-    return <Navigate to="/" replace />; // Arahkan ke halaman login jika tidak memenuhi syarat
+    return <Navigate to="/login" replace />;
   }
 
   return children;
